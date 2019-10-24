1c7da02aabc26e9fcce3ec54ee59bd50e7b4b3e3
Dumisani <dumisanik.shabangu@gmail.com>
2019-10-24 18:39:29 +0200

    Added flask-migrate and flask bcrypt

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -6,25 +6,30 @@ import os
 
 class BaseConfig:
     """Base configuration"""
+    DEBUG = False  # new
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'my_precious'
     DEBUG_TB_ENABLED = False
     DEBUG_TB_INTERCEPT_REDIRECTS = False
+    BCRYPT_LOG_ROUNDS = 13  # new
 
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     DEBUG_TB_ENABLED = True
+    BCRYPT_LOG_ROUNDS = 4  # new
 
 
 class TestingConfig(BaseConfig):
     """Testing configuration"""
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
+    BCRYPT_LOG_ROUNDS = 4  # new
 
 
 class ProductionConfig(BaseConfig):
     """Production configuration"""
+    DEBUG = False  # new
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
